738aee9694988cef0b7acfa99b00d0caee04ee95
Barak Dimand <barakdimand@aol.com>
2020-09-28 10:42:36 -0700

    starting the Contact Me column in the Contact component

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Barak Dimand</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              {" "}
+              This is all about Barak and how amazing he is cuz hes been number
+              one forever
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+          </Cell>
+        </Grid>
       </div>
     );
   }

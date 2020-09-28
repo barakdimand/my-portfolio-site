b47914de2fc2a6771520b1c21dce4023915e7a51
Barak Dimand <barakdimand@aol.com>
2020-09-27 23:12:39 -0700

    first commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LandingPage extends LandingPage {
+  render() {
+    return (
+      <div>
+        <h1>LandingPage</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
